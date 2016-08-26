635d3b790d84baa628b1eaa6e455b4aa317eec08
runforever <c.chenchao.c@gmail.com>
2016-08-26 17:12:36 +0800

    init project

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    website: {
+        assets: './assets',
+        js: [
+            'plugin.js'
+        ],
+        css: [
+            'http://yui.yahooapis.com/pure/0.6.0/pure-min.css',
+            'plugin.css'
+        ]
+    }
+};
